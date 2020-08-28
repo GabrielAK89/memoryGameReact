5b1d991183bd2d23d5656097b0c7b2799acaa752
Gabriel Acatrinei <gabrielak89@gmail.com>
2020-08-28 18:42:04 +0300

    Initial commit

---
+++ src/Components/Images/index.js
@@ -0,0 +1,63 @@
+import Cangur from "./cangur.PNG";
+import Elefant from "./elefant.PNG";
+import Girafa from "./girafa.PNG";
+import Koala from "./koala.PNG";
+import Leopard from "./leopard.PNG";
+import onePiece from "./back-image.PNG";
+
+export let images = [
+  {
+    name: "Cangur",
+    pic: Cangur,
+    flipped: false,
+  },
+  {
+    name: "Elefant",
+    pic: Elefant,
+    flipped: false,
+  },
+  {
+    name: "Girafa",
+    pic: Girafa,
+    flipped: false,
+  },
+  {
+    name: "Koala",
+    pic: Koala,
+    flipped: false,
+  },
+  {
+    name: "Leopard",
+    pic: Leopard,
+    flipped: false,
+  },
+
+  {
+    name: "Cangur",
+    pic: Cangur,
+    flipped: false,
+  },
+  {
+    name: "Elefant",
+    pic: Elefant,
+    flipped: false,
+  },
+  {
+    name: "Girafa",
+    pic: Girafa,
+    flipped: false,
+  },
+  {
+    name: "Koala",
+    pic: Koala,
+    flipped: false,
+  },
+  {
+    name: "Leopard",
+    pic: Leopard,
+    flipped: false,
+  },
+
+];
+
+export const blankCard = onePiece;
